d9028882fc8a7bd03afc7144c5e97b7b90e90577
Kolozuz <moralesjuanpablo1122@gmail.com>
2023-09-01 02:28:20 -0500

    moved all the logic to functions

---
+++ test.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from fns import start, login, nose
+
+# driver = webdriver.Chrome()
+
+# #def start():
+# driver.implicitly_wait(300)
+
+start()
+login()
+nose()
+
+
+
+
