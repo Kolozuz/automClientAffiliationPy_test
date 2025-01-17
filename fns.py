96b564476bb15448d9526fca5070d323292436b3
Kolozuz <moralesjuanpablo1122@gmail.com>
2025-01-17 16:04:50 -0500

    feat: noknow, esto e muy viejo

--- fns.py
+++ fns.py
@@ -7,46 +7,84 @@ from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 
+# Loads OIPA web page in the current browser session,      
+# and sets the implicit wait to 5 seconds (time to      
+# find each element)   
 
+class webPage(object) :
+    """
+    Instantiates a new webpage, with a given url parameter
+    """
+    def __init__(self, url):
+        self.url = url
+
+class loginPage(webPage) :
+    """
+    yomama so fat
+    """
+    urltext : str
+
+    def geturl(urltext):
+        driver.get(urltext)
+
+    def login(user_name = "", user_password = ""):
+        """
+        Takes username and password parameters to sign in and returns a success message,
+        if no parameters are given it asks for them in console
+
+        Args:
+            user_name (str): user name.
+            user_password (str): password.
+
+        Returns:
+            str: result message.
+        """
+
+        driver.implicitly_wait(5)
+        form = driver.find_element(By.ID, "form-container") 
+        user_name_input = driver.find_element(By.ID, "userName")
+        user_password_input = driver.find_element(By.ID, "password")
+        submit_btn = driver.find_element(By.ID, "submit")
+
+
+        def checkCredentials(user_name = "", user_password = "") :
+
+            while not user_name or not user_password : 
+                user_name = input("Type your Username \n")
+                user_password = input("Type your Password \n")
+                user_name_input.clear()
+                user_password_input.clear()
+                user_name_input.send_keys(user_name)
+                user_password_input.send_keys(user_password)
+                submit_btn.click()
+
+                homePage = driver.find_elements(By.ID, "homePage")
+                
+                errorMessage = driver.find_elements(By.CSS_SELECTOR, "span[data-bind='text:$parent.errorMessage()']")
+
+                if len(homePage) == 0 : # While error message is present
+                    print("Wrong Username or Password! Try Again")
+                    user_name = ""
+                    user_password = ""
+                else:
+                    print("si")
+
+        # if user_name_input.is_displayed() and user_password_input.is_displayed() :
+        checkCredentials(user_name, user_password)
+        #     user_name_input.send_keys(user_name)
+        #     user_password_input.send_keys(user_password)
+        #     form.submit()
+    
+        return successmessage
+    
 def start():
+    #kk = loginPage.url
+    #print(kk)
     driver.implicitly_wait(5)
     driver.get("http://corevida.dllosura.com/PASJava/index.html")
-
-def login(user_name = "", user_password = ""):
-    # driver.implicitly_wait(30)
-    form = driver.find_element(By.ID, "form-container") 
-    user_name_input = driver.find_element(By.ID, "userName")
-    user_password_input = driver.find_element(By.ID, "password")
-    submit_btn = driver.find_element(By.ID, "submit")
+    pass
 
 
-    def checkCredentials(user_name = "", user_password = "") :
-
-        while not user_name or not user_password : 
-            user_name = input("Type your Username \n")
-            user_password = input("Type your Password \n")
-            user_name_input.clear()
-            user_password_input.clear()
-            user_name_input.send_keys(user_name)
-            user_password_input.send_keys(user_password)
-            submit_btn.click()
-
-            homePage = driver.find_elements(By.ID, "homePage")
-            
-            errorMessage = driver.find_elements(By.CSS_SELECTOR, "span[data-bind='text:$parent.errorMessage()']")
-
-            if len(homePage) == 0 : # While error message is present
-                print("Wrong Username or Password! Try Again")
-                user_name = ""
-                user_password = ""
-
-    # if user_name_input.is_displayed() and user_password_input.is_displayed() :
-    checkCredentials(user_name, user_password)
-    #     user_name_input.send_keys(user_name)
-    #     user_password_input.send_keys(user_password)
-    #     form.submit()
-  
-    return print("success!")
 
 def nose():
      dropdown_create = driver.find_element(By.ID, "ojChoiceId_Menu_CreateNew")
--- test.py
+++ test.py
@@ -1,16 +1,21 @@
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-from fns import start, login, nose
+from fns import webPage, loginPage, nose
 
-# driver = webdriver.Chrome()
+myPage = webPage("http://corevida.dllosura.com/PASJava/index.html")
 
-# #def start():
-# driver.implicitly_wait(300)
+URLSTR = myPage.url
 
-start()
-login()
-nose()
+myloginPage = loginPage(myPage)
+myloginPage.urltext = URLSTR
+print(URLSTR)
 
+#start() #   Loads web page (Oipa)
 
+myloginPage.geturl()
 
+myloginPage.login() #   Asks for login credentials (username & password) in console.    
+                  #   Alternatively you can send them as parameters like this ->  
+                  #   login("your_username", "your_password") so that you don't 
+                  #   have to type them every time you run the script
 
+nose()  #   Opens the form, fills the fields and creates a natural client
